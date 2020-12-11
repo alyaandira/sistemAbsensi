24133af14657ff6f2827aad2330f048dc594716b
alyaandira <75844516+alyaandira@users.noreply.github.com>
2020-12-11 21:04:56 +0700

    Initial Commit
    First time submit

---
+++ db-component/config.php
@@ -0,0 +1,35 @@
+<?php
+
+// ====== All PHP variables regarding database configuration are listed down here ======
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$dbName = "sispeg";
+
+// Departement Tables
+$department_table = "department";
+$department_id = "department_id";
+$department_name = "department_name";
+//listdown semua table row nya (data)
+
+// Employee Tables
+$employee_table = "employee";
+$employee_NIP = "employee_NIP";
+$employee_fullname = "employee_fullname";
+$employee_password = "employee_password";
+$employee_email = "employee_email";
+$employee_address = "employee_address";
+$position_id = "position_id";
+
+// Position Tables
+$position_table = "position";
+$position_id = "position_id";
+$position_name = "position_name";
+$department_id = "department_id";
+
+// Presence Tables
+$presence_table = "presence";
+$presence_id = "presence_id";
+$presence_dateTime = "presence_dateTime";
+$presence_status = "presence_status";
+$employee_NIP = "employee_NIP";
---
+++ login.php
@@ -0,0 +1,70 @@
+<?php
+session_start();
+session_unset();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Attendance System</title>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;700&display=swap" rel="stylesheet">
+
+    <!-- Font Icon -->
+    <link rel="stylesheet" href="fonts/material-icon/css/material-design-iconic-font.min.css">
+
+    <!-- Main css -->
+    <link rel="stylesheet" href="css/style.css">
+    <script src="src\izitoast\dist\js\iziToast.js" type="text/javascript"></script>
+    <link rel="stylesheet" href="src\izitoast\dist\css\iziToast.css">
+
+    <!-- JS -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="js/main.js"></script>
+</head>
+
+<body>
+
+    <?php
+    if (isset($_POST["user_name"])) {
+        include 'db-component\employee-login.php';
+    }
+    ?>
+    <!-- <img src="./source/image/image.jpg" alt=""> -->
+    <div class="main">
+
+        <!-- Sing in  Form -->
+        <section class="sign-in">
+            <div class="container">
+                <div class="signin-content">
+                    <div class="signin-image">
+                        <figure><img src="images/signin-image.jpg" alt="sing up image"></figure>
+                        <!-- <a href="#" class="signup-image-link">Create an account</a> -->
+                    </div>
+
+                    <div class="signin-form">
+                        <h2 class="form-title">Log In</h2>
+                        <form method="POST" class="register-form" id="login-form">
+                            <div class="form-group">
+                                <label for="your_name"><i class="zmdi zmdi-account material-icons-name"></i></label>
+                                <input type="text" name="user_name" id="your_name" placeholder="NIP" />
+                            </div>
+                            <div class="form-group">
+                                <label for="your_pass"><i class="zmdi zmdi-lock"></i></label>
+                                <input type="password" name="user_pass" id="your_pass" placeholder="Password" />
+                            </div>
+                            <div class="form-group form-button">
+                                <input type="submit" name="signin" id="signin" class="form-submit" value="Log in" />
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</body>
+
+</html>
