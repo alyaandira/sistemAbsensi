9954b0c846845e4b990bdf551ae43e370f167521
alyaandira <75844516+alyaandira@users.noreply.github.com>
2021-08-27 21:10:31 +0700

    facerecog

--- daftarKelas.php
+++ daftarKelas.php
@@ -11,7 +11,7 @@ session_start();
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="description" content="">
-  <meta name="author" content="Alyen Andiren">
+  <meta name="author" content="Alyen Andira">
   <!-- Favicon icon -->
   <link rel="icon" type="image/png" sizes="16x16" href="./assets/images/favicon.png">
   <title>Sistem Absensi - Pindai Absensi</title>
